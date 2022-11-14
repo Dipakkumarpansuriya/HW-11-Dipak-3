f0f676245f959f59f31a075792769205ba9c7a7b
primetraineesix <primetraineesix@gmail.com>
2022-11-14 10:47:05 +0000

    d

---
+++ com-herokuapp-Dipak-3/src/browsers/Multy.java
@@ -0,0 +1,42 @@
+package browsers;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.time.Duration;
+
+public class Multy {
+
+
+    static String browser = "edge";
+    static String baseUrl = "http://the-internet.herokuapp.com/login";
+    static WebDriver driver;
+
+    public static void main(String[] args) {
+        if (browser.equalsIgnoreCase("chrome")) {
+            System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
+            driver = new ChromeDriver();
+
+        } else if (browser.equalsIgnoreCase("firefox")) {
+            System.setProperty("webdriver.firefox.driver", "drivers/firefoxdriver.exe");
+            driver = new FirefoxDriver();
+
+        } else if (browser.equalsIgnoreCase("edge")) {
+            System.setProperty("webdriver.edge.driver", "drivers/msedgedriver.exe");
+            driver = new EdgeDriver();
+
+        }else {
+            System.out.println("Wrong Browser Name");
+        }
+        driver.get(baseUrl);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+
+        //close the browser
+        driver.quit();
+
+
+    }
+}
